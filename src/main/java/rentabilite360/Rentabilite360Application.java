0f397f69105e36ec750b8bbd152daec222e89bfd
achrafelaffas <achrafelaffas@gmail.com>
2024-04-22 21:17:18 +0100

    Init

---
+++ src/main/java/rentabilite360/Rentabilite360Application.java
@@ -0,0 +1,13 @@
+package rentabilite360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rentabilite360Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rentabilite360Application.class, args);
+    }
+
+}
---
+++ src/test/java/rentabilite360/Rentabilite360ApplicationTests.java
@@ -0,0 +1,13 @@
+package rentabilite360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rentabilite360ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
