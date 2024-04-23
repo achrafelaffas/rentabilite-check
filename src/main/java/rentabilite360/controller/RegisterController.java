c8ef383d0a161f1412f55d5e6d3a648e11c2d392
achrafelaffas <achrafelaffas@gmail.com>
2024-04-23 19:42:07 +0100

    Creating dashboard and auth pages

---
+++ src/main/java/rentabilite360/controller/RegisterController.java
@@ -0,0 +1,11 @@
+package rentabilite360.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class RegisterController {
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
+    }
+}
