eac315c46d574dd2fa25c4c306fe611ce6900279
achrafelaffas <achrafelaffas@gmail.com>
2024-04-28 14:44:08 +0100

    update index.html

--- src/main/java/rentabilite360/Rentabilite360Application.java
+++ src/main/java/rentabilite360/Rentabilite360Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Rentabilite360Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Rentabilite360Application.class, args);
     }
-
 }
